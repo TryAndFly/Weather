d3ddb45d64e1e04a98c1735c1eff9fd1d5c66159
TryAndFry <461907111@qq.com>
2016-07-20 15:12:31 +0800

    修复IO错误

--- app/src/main/java/com/example/weather/util/HttpUtil.java
+++ app/src/main/java/com/example/weather/util/HttpUtil.java
@@ -12,30 +12,36 @@ import java.net.URL;
  * Created by Administrator on 2016/7/18.
  */
 public class HttpUtil {
+    private static final int BUFFER_SIZE=1024*1024;
     public static void sendHttpRequest(final String address ,final HttpCallbackListener listener){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                HttpURLConnection httpURLConnection=null;
+                HttpURLConnection connection=null;
                 URL url = null;
                 try {
                     url = new URL(address);
-                    httpURLConnection = (HttpURLConnection)url.openConnection();
-                    httpURLConnection.setRequestMethod("GET");
-                    httpURLConnection.setConnectTimeout(8000);
-                    httpURLConnection.setReadTimeout(8000);
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    //将输入流转换成成缓冲流
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String string;
-                    while ((string = bufferedReader.readLine()) != null){
-                        stringBuilder.append(string);
+                    connection = (HttpURLConnection)url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+
+                    InputStream in = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+//                    String line;
+//                    while ((line = reader.readLine()) != null) {
+//                        response.append(line);
+//                    }
+                    char[] data = new char[BUFFER_SIZE];
+                    int len  = reader.read(data);
+                    response.append(data,0,len);
+                    if (reader!=null){
+                        reader.close();
                     }
                     //
-//                    Log.d("HttpUtil",stringBuilder.toString());正常获取
                     if (listener!=null){
-                        listener.onFinish(stringBuilder.toString());
+                        listener.onFinish(response.toString());
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -43,8 +49,8 @@ public class HttpUtil {
                         listener.onError(e);
                     }
                 }finally {
-                    if (httpURLConnection!=null){
-                        httpURLConnection.disconnect();
+                    if (connection!=null){
+                        connection.disconnect();
                     }
                 }
             }
